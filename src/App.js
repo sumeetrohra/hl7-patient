8a33d7a58b02f9edf578c4f76779ad49d6ba5aef
sumeetrohra <sumeetrohra1@gmail.com>
2020-03-28 18:31:40 +0530

    remove serviceWorker and change styles

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
     <BrowserRouter>
       <ApolloProvider client={client}>
         <Header />
-        <Container>
+        <Container style={{ marginBottom: '30px' }}>
           <Router />
         </Container>
       </ApolloProvider>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AuthConfig from './AuthConfig';
@@ -12,8 +11,3 @@ ReactDOM.render(
   </AuthConfig>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
